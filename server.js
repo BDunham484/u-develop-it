766dad650c0a6684111d3e2280d5b871bd37ebb2
ThereWolf <bdunham484@gmail.com>
2022-07-19 13:38:00 -0500

    connect sql to node

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,11 +7,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//     res.json({
-//         message: 'Hello World!'
-//     });
-// });
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username,
+        user: 'root',
+        //your mysql password
+        password: '+g47|lj-Anzn-<LUQ;?xaK63>qv}',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+})
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
